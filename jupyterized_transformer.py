50156410b114121ae823e8de2271998312163c3c
Sithyphus <jorgebarrios@email.arizona.edu>
2019-11-26 14:24:10 -0700

    Only thing left is to build the docker image

---
+++ jupyterized_transformer.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Notebook for Creating Transformer Algorithms
+
+# Import any needed modules here
+
+# In[ ]:
+
+
+import numpy as np
+
+
+# ## Definitions
+# Please replace these definition's values with the correct ones
+
+# In[ ]:
+
+
+VERSION = ''
+
+
+# ## Creator Information
+# Enter Author Name
+
+# In[ ]:
+
+
+ALGORITHM_AUTHOR = ''
+
+
+# Enter Author Email
+
+# In[ ]:
+
+
+ALGORITHM_AUTHOR_EMAIL = ''
+
+
+# Enter Algorithm Name
+
+# In[ ]:
+
+
+ALGORITHM_AUTHOR_EMAIL = ''
+
+
+# Enter Algorithm Description
+
+# In[ ]:
+
+
+ALGORITH_DESCRIPION = ''
+
+
+# ## Citation information for publication (More information in HOW_TO.md)
+
+# In[ ]:
+
+
+CITATION_AUTHOR = ''
+CITATION_TITLE = ''
+CITATION_YEAR = ''
+
+
+# ### The name of one or more variables returned by the algorithm, seperated by commas (more info in HOW_TO.md)
+# If only one name is specified, no commas are used.
+# Note that variable names cannot have commas in them: use a different separator instead. Also, all white space is kept intact; don't add any extra white space since it may cause name comparisons to fail.
+
+# In[ ]:
+
+
+VARIABLE_NAMES = ''
+
+
+# ### Optional override for the generation of a BETYdb compatible csv
+# Set to false to suppress the creation of a compatible file
+
+# In[ ]:
+
+
+WRITE_BETYDB_CSV = True
+
+
+# ### Optional override for the generation of a TERRA REF Geostreams compatible csv file
+# Set to False to suppress the creation of a compatible file
+
+# In[ ]:
+
+
+WRITE_GEOSTREAMS_CSV = True
+
+
+# ## Entry point for plot level RBG algorithm
+
+# In[ ]:
+
+
+def calculate(pxarray: np.ndarray):
+    """"Calculates one or more values from plot level RGB data
+    Arguments:
+        pxarray: Array of RGB data for a single plot
+    Return:
+        Returns one or more calculated values
+    """
+
+
+# ### ALGORITHM: Enter your algorithm here
+
+# In[ ]:
+
+
+
+
+
+# ### RETURN: Enter your calculated values. Be sure to order them as defined in VARIABLE_NAMES above
+
+# In[ ]:
+
+
+return 
+
+
+# In[ ]:
+
+
+print("hello_world")
+
